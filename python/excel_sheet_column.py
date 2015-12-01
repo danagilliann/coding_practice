bd735dd054df4fdb7c63c045c738aaaaf0deeb2c
danagilliann <danalee96@gmail.com>
2015-11-30 22:49:48 -0500

    excel sheet problem

--- python/excel_sheet_column.py
+++ python/excel_sheet_column.py
@@ -1,11 +1,28 @@
-def excel_sheet(c): 
-        c = c.lower()
-        a_ascii = ord("a")
-        z_ascii = ord("z")
+'''
 
-        if len(c) == 1:
-                return (ord(c) - a_ascii + 1)
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+For example:
+   A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+
+'''
+
+def excel_sheet(s): 
+        s = s.lower()
+        a_ascii = ord('a')
+        z_ascii = ord('z')
+        ret = 0
+        char = ""
+
+        if len(s) == 1:
+                return (ord(s) - a_ascii + 1)
         else:
-            return ((z_ascii - a_ascii + 1) * (len(c) - 1)) + (ord(c[len(c)-1]) - a_ascii + 1)
-
-print excel_sheet("BBA")
+                for c in range(0, len(s)):
+                        char = s[c]
+                        ret += ((26 ** (len(s) - c - 1)) * (ord(char) - a_ascii + 1))
+                return ret
